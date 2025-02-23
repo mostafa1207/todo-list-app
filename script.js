4eb8a83388ae557717c7acd5f6d84df364b4ade4
mostafa1207 <mostafaabdelaal22@gmail.com>
2025-02-23 09:14:57 +0200

    Refactored task rendering to use DOM API instead of innerHTML

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let tasks = [
+const tasks = [
   {
     title: "قراءة كتاب",
     date: "1/1/2001",
@@ -16,32 +16,48 @@ let tasks = [
   },
 ];
 
-function createTasksContent() {
-  let content = "";
-  for (const task of tasks) {
-    content += `
-        <!-- task -->
-        <div class="task">
-            <!-- task info -->
-            <div class="task-info">
-                <p>${task.title}</p>
-                <span>${task.date}</span>
-            </div>
-            <!--// task info //-->
-    
-            <!-- task actions -->
-            <div class="task-actions">
-                <button class="circular-btn">❌</button>
-                <button class="circular-btn">✅</button>
-                <button class="circular-btn">✏️</button>
-            </div>
-            <!--// task actions //-->
-        </div>
-        <!--// task //-->
-    `;
+function createBtns() {
+  const btns = [];
+  for (let i = 0; i < 3; ++i) {
+    const btn = document.createElement("button");
+    btn.classList.add("circular-btn");
+    btns.push(btn);
   }
-  return content;
+  return btns;
 }
 
-const content = createTasksContent();
-document.getElementById("tasks").innerHTML = content;
+function createTasksDivs() {
+  const tasksDivs = [];
+  for (const task of tasks) {
+    const taskTitle = document.createElement("p");
+    taskTitle.textContent = `${task.title}`;
+
+    const taskDate = document.createElement("span");
+    taskDate.textContent = `${task.date}`;
+
+    const taskInfo = document.createElement("div");
+    taskInfo.classList.add("task-info");
+    taskInfo.append(taskTitle, taskDate);
+
+    const btns = createBtns();
+    btns[0].textContent = "❌";
+    btns[1].textContent = "✅";
+    btns[2].textContent = "✏️";
+
+    const taskActions = document.createElement("div");
+    taskActions.classList.add("task-actions");
+    taskActions.append(...btns);
+
+    const taskDiv = document.createElement("div");
+    taskDiv.classList.add("task");
+    taskDiv.append(taskInfo, taskActions);
+
+    tasksDivs.push(taskDiv);
+  }
+  return tasksDivs;
+}
+
+const tasksContainer = document.getElementById("tasks");
+tasksContainer.innerHTML = "";
+const tasksDivs = createTasksDivs();
+tasksContainer.append(...tasksDivs);
