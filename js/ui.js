fbc5a63ae047366b65e6541d2826228a2e0425c5
mostafa1207 <mostafaabdelaal22@gmail.com>
2025-02-24 08:49:35 +0200

    Refactored createTasksDivs and createBtns for cleaner and more efficient code

--- js/ui.js
+++ js/ui.js
@@ -1,18 +1,18 @@
 import { tasks } from "./tasks.js";
 
 function createBtns() {
-  const btns = [];
-  for (let i = 0; i < 3; ++i) {
+  const btnTexts = ["❌", "✅", "✏️"];
+  const btns = btnTexts.map((text) => {
     const btn = document.createElement("button");
     btn.classList.add("circular-btn");
-    btns.push(btn);
-  }
+    btn.textContent = text;
+    return btn;
+  });
   return btns;
 }
 
 function createTasksDivs() {
-  const tasksDivs = [];
-  for (const task of tasks) {
+  const tasksDivs = tasks.map((task) => {
     const taskTitle = document.createElement("p");
     taskTitle.textContent = `${task.title}`;
 
@@ -23,21 +23,17 @@ function createTasksDivs() {
     taskInfo.classList.add("task-info");
     taskInfo.append(taskTitle, taskDate);
 
-    const btns = createBtns();
-    btns[0].textContent = "❌";
-    btns[1].textContent = "✅";
-    btns[2].textContent = "✏️";
-
     const taskActions = document.createElement("div");
     taskActions.classList.add("task-actions");
+    const btns = createBtns();
     taskActions.append(...btns);
 
     const taskDiv = document.createElement("div");
     taskDiv.classList.add("task");
     taskDiv.append(taskInfo, taskActions);
 
-    tasksDivs.push(taskDiv);
-  }
+    return taskDiv;
+  });
   return tasksDivs;
 }
 
